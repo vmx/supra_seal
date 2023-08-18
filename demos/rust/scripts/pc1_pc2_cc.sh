f3b8afcbfca39a8c89361b3217dc8dc8a49550fc
Volker Mische <volker.mische@gmail.com>
2023-08-18 11:27:57 +0200

    feat: add binary for TreeC

--- demos/rust/scripts/pc1_pc2_cc.sh
+++ demos/rust/scripts/pc1_pc2_cc.sh
@@ -14,6 +14,7 @@ It prints to stdout the result CommR formatted as JSON, e.g.
 {"comm_r":"0x9dabeaa4e2b53153152ac485c6b8ede4d750be12d0fae4fa265161dc0ff5502a"}
 
 The input parameters are given by piping in JSON with the following keys:
+ - output_dir: The directory where all files (e.g. the trees) are stored.
  - parents_cache_path: The path to the parents cache file. For 32GiB sectors, that's usually at
    /var/tmp/filecoin-parents/v28-sdr-parent-55c7d1e6bb501cc8be94438f89b577fddda4fafa71ee9ca72eabe2f0265aefa6.cache.
  - replica_ids: A list of Replica IDs formatted in hex with leading 0x.
@@ -21,6 +22,7 @@ The input parameters are given by piping in JSON with the following keys:
 
 Example JSON:
 {
+  "output_dir": "/path/to/some/dir",
   "parents_cache_path": "/path/to/filecoin-parents/v28-sdr-parent-abcdef.cache",
   "replica_ids": [
     "0xd93f7c0618c236179361de2164ce34ffaf26ecf3be7bf7e6b8f0cfcf886ad0d0",
@@ -54,21 +56,31 @@ fi
 
 # Parse the input data.
 read -r input_args
+output_dir=$(echo "${input_args}" | ${JQ} '.output_dir')
 parents_cache_path=$(echo "${input_args}" | ${JQ} '.parents_cache_path')
 # Parse Replica ID's in a format that `jo` understands: '-s 0xaa -s 0xbb'
 replica_ids=$(echo "${input_args}" | ${JQ} '.replica_ids | @sh' | tr -d "'" | sed -e 's/0x/-s 0x/g')
+num_sectors=$(echo "${input_args}" | ${JQ} '.replica_ids | length')
 supraseal_config_path=$(echo "${input_args}" | ${JQ} '.supraseal_config_path')
 
+if [ "${output_dir}" = 'null' ]; then
+    echo "'output_dir' not set." && exit 4
+fi
 if [ "${parents_cache_path}" = 'null' ]; then
-    echo "'parents_cache_path' not set." && exit 4
+    echo "'parents_cache_path' not set." && exit 5
 fi
 if [ "${replica_ids}" = 'null' ]; then
-    echo "'replica_ids' not set." && exit 5
+    echo "'replica_ids' not set." && exit 6
 fi
 if [ "${supraseal_config_path}" = 'null' ]; then
-    echo "'supraseal_config_path' not set." && exit 6
+    echo "'supraseal_config_path' not set." && exit 7
 fi
 
 # Run SDR.
-sdr=$(${JO} parents_cache_path="${parents_cache_path}" replica_ids=$(jo -a -- ${replica_ids}) -s supraseal_config_path="$supraseal_config_path" | ${CARGO} --bin sdr)
+sdr=$(${JO} -s parents_cache_path="${parents_cache_path}" replica_ids=$(jo -a -- ${replica_ids}) -s supraseal_config_path="$supraseal_config_path" | ${CARGO} --bin sdr)
 >&2 echo "SDR: ${sdr}"
+
+# Run TreeC.
+tree_c=$(${JO} num_sectors="${num_sectors}" -s output_dir="${output_dir}" -s supraseal_config_path="$supraseal_config_path" | ${CARGO} --bin tree-c)
+>&2 echo "TreeC: ${tree_c}"
+
---
+++ demos/rust/src/bin/tree-c.rs
@@ -0,0 +1,55 @@
+use std::fmt;
+
+use anyhow::Result;
+use log::info;
+use serde::{Deserialize, Serialize};
+use serde_hex::{SerHex, StrictPfx};
+
+use supra_seal_demo::cli;
+
+#[derive(Deserialize, Serialize)]
+#[serde(transparent)]
+struct Bytes(#[serde(with = "SerHex::<StrictPfx>")] [u8; 32]);
+
+impl fmt::Debug for Bytes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "0x{}", hex::encode(self.0))
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct TreeCParameters {
+    /// The number of sectors to process in parallel.
+    num_sectors: usize,
+    /// Directory where the resulting TreeC is stored.
+    output_dir: String,
+    /// Path to a Supraseal configutation file.
+    supraseal_config_path: String,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct TreeCOutput {
+    // This is a hack to serialize a struct into an empty Object instead of null
+    #[serde(skip_serializing)]
+    _placeholder: (),
+}
+
+// NOTE vmx 2023-08-17: For now we always start at the beginning of the NVMes, I currently don't
+// see a reason not to.
+const BLOCK_OFFSET: usize = 0;
+
+//fn main() -> Result<()> {
+fn main() -> Result<()> {
+    fil_logger::maybe_init();
+
+    let params: TreeCParameters = cli::parse_stdin()?;
+    info!("{:?}", params);
+
+    supra_seal_demo::init(params.supraseal_config_path);
+    supra_seal_demo::pc2(BLOCK_OFFSET, params.num_sectors, params.output_dir);
+
+    let output = TreeCOutput::default();
+    cli::print_stdout(output)?;
+
+    Ok(())
+}
